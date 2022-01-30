52dad7b487d5e6b9484e1cba7444920e3e29db25
Yermek <ayerabdi@gmail.com>
2022-01-31 03:13:09 +0600

    done controllers

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 const TelegaApi = require('node-telegram-bot-api')
 
+const {gameOptions, againOptions} = require('./options.js')
+
 const token = '5291290036:AAFbeCPZ_j3FkSi8xz-Qx1l9UMDjRCOV_1A'
 
 const bot = new TelegaApi(token,{polling:true})
 
 const chats ={}
 
-const gameOptions = {
-    reply_markup: JSON.stringify({
-        inline_keyboard:[
-            [
-                {text:"1", callback_data: "1"},{text:"1.1", callback_data: "1.1"},{text:"1.2", callback_data: "1.2"}
-            ],
-            [
-                {text:"2", callback_data: "2"}
-            ],
-            [
-                {text:"3", callback_data: "3"}
-            ],
-            [
-                {text:"4", callback_data: "4"}
-            ],
-            [
-                {text:"5", callback_data: "5"}
-            ]
-        ]
-    })
+const startGame = async (chatId) =>{
+            await bot.sendMessage(chatId,"В общем игра началась, я загадал число от 1 до 7, угадай")
+            const randomNumber = Math.floor(Math.random()*7)
+            chats[chatId] = randomNumber
+            await bot.sendMessage(chatId,"Отгадывай",gameOptions)
 }
+
 const start = () => {
     bot.setMyCommands([
-        {command : '/start', description : 'Привет, выбери криптовалюту, покажу сколбко это долларов сейчас за один токен'},
+        {command : '/start', description : 'Привет, выбери криптовалюту, покажу сколько это долларов сейчас за один токен'},
         {command : '/choose', description : 'Показать топ криптовалют'},
         {command : '/game', description : 'Игра отгадай число'}
     ])
@@ -44,19 +32,23 @@ const start = () => {
             return bot.sendMessage(chatId,"Список топов")
         }
         if(text =='/game'){
-            await bot.sendMessage(chatId,"В общем игра началась, я загадал число от 1 до 9, угадай")
-            const randomNumber = Math.floor(Math.random()*10)
-            chats[chatId] = randomNumber
-            return bot.sendMessage(chatId,"Отгадывай",gameOptions)
+            return startGame(chatId)
         }
         return bot.sendMessage(chatId,'Я вас не понял')
         
     })
-    bot.on('callback_query',msg => {
+    bot.on('callback_query', async msg => {
         const data = msg.data
         const chatId = msg.message.chat.id
-        bot.sendMessage(chatId, `Ты выбрал цифру ${data}`)
-        console.log(msg)
+        if(data === '/again'){
+            return startGame(chatId)
+        }
+        if(data === chats[chatId]){
+            return await bot.sendMessage(chatId, "Ты угадал!",againOptions)
+        }
+        else{
+            return await bot.sendMessage(chatId, `Не угадал, бот загадал ${chats[chatId]}`,againOptions)
+        }
     })
 }
 
