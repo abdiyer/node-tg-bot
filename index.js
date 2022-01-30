43fd5d4124a2d5e252203e63ddb17adc49bae3b3
Yermek <ayerabdi@gmail.com>
2022-01-30 21:12:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const token = '5291290036:AAFbeCPZ_j3FkSi8xz-Qx1l9UMDjRCOV_1A'
 
 const bot = new TelegaApi(token,{polling:true})
 
+bot.setMyCommands([
+    {command : '/start', description : 'Привет, выбери криптовалюту, покажу сколбко это долларов сейчас за один токен'},
+    {command : '/showTokens', description : 'Показать топ криптовалют'}
+])
+
 bot.on('message', msg => {
     const text = msg.text
-    const chatId = msg.chatId
-    console.log(text,chatId)
+    const chatId = msg.chat.id
+    if(text =='/start'){
+        bot.sendMessage(chatId,"Добро пожаловать")
+    }
 })
